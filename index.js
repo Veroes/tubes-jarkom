321379b38e60ab5fa44d252ed3de4d5aa9621c93
Arifki Ilham <79450046+Veroes@users.noreply.github.com>
2023-05-05 16:46:03 +0700

    update again

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Test")
+const submit = document.getElementById('client-thoughts');
+submit.addEventListener('click', function() {
+    alert("Succesfully give a message for us, Thank you for the comments");
+})
